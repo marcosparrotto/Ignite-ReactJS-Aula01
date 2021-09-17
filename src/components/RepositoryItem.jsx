b7cac5f5dfc46e49099528aa2da80d5e56b34358
Marcos Parrotto <marcos.parrotto@gmail.com>
2021-09-16 22:22:25 -0300

    Proriedades e Estado do componente

---
+++ src/components/RepositoryItem.jsx
@@ -0,0 +1,11 @@
+export function RepositoryItem(props){
+  return (
+    <li>
+      <strong>{props.repository?.name ?? 'Default'}</strong>
+      <p>{props.repository?.description ?? 'Default'}</p>
+      <a href= {props.repository?.link ?? 'Default'}>
+        Acessar repositórios
+      </a>
+    </li>
+  );
+}
